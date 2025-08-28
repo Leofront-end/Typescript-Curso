76edcee39308544753e6294dfbce559be20edb90
Leofront-end <ls171258@gmail.com>
2025-08-28 17:27:46 -0300

    Funções

---
+++ Funções/funcoes.ts
@@ -0,0 +1,9 @@
+function soma(num1: number, num2:number): number {
+    return num1+num2
+}
+
+console.log(soma(3,5));
+
+const soma2 = (num1: number, num2: number):number => num1+num2
+
+console.log(soma2(4,5));
